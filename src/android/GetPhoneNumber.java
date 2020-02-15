6ff9f21e5e8b5e1ef7d2f616b0f06dd52c9431f7
frizalid <rizalfadli975@gmail.com>
2020-02-15 09:40:26 +0700

    update

--- src/android/GetPhoneNumber.java
+++ src/android/GetPhoneNumber.java
@@ -16,8 +16,7 @@ public class GetPhoneNumber extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
         if (action.equals("get")) {
-            TelephonyManager telephonyManager =
-                (TelephonyManager)this.cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+            TelephonyManager telephonyManager = (TelephonyManager)this.cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
             String result = telephonyManager.getLine1Number();
             if (result != null) {
                 callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
